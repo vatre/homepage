48a8d97d307f0ab90906616c19a3cb7ac68587ec
vatre <trean.v@gmail.com>
2016-12-21 22:46:46 +0100

    WIP popin

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var popupMapping = {
+        "c": "calculator",
+    }
+
+    function openPopupContainer() {
+        $("#popup-container").css('display', 'initial');
+    }
+
+    function openPopup(key) {
+        var sel = "#" + popupMapping[key];
+        $(sel).css('display', 'initial');
+    }
+});

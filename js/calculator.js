a23ee5c4963e463c4d152eab923ca3b1b410a24c
trean.v <trean.v@gmail.com>
2016-12-22 10:58:56 -0500

    New calculator version, one bug to fix

--- js/calculator.js
+++ js/calculator.js
@@ -43,206 +43,121 @@ $(document).ready(function() {
     }
     return output;
   }
-
-  //https://code.tutsplus.com/articles/data-structures-with-javascript-tree--cms-23393
-
-  function Node(data) {
-    var self = this;
-    self.data = data;
-    self.parent = null;
-    self.leftChild = null;
-    self.rightChild = null;
-
-    self.addChild = function(node) {
-      if (self.leftChild === null) {
-        self.leftChild = node;
-      } else if (self.rightChild === null) {
-        self.rightChild = node;
-      } else {
-        throw "Tried to add a child to a node which already has two.";
-      }
-    }
-  }
-
-  function Tree(data) {
-    var self = this;
-    var node = new Node(data);
-    self._root = node;
-
-    self.traverseDF = function(callback) {
-      (function recurse(currentNode) {
-
-        if (currentNode.leftChild !== null) {
-          recurse(currentNode.leftChild);
-        }
-        if (currentNode.rightChild !== null) {
-          recurse(currentNode.rightChild);
-        }
-
-        callback(currentNode);
-
-      })(self._root);
-    }
-
-    self.traverseBF = function(callback) {
-      var queue = [self._root];
-
-      while (queue.length > 0) {
-        var newQueue = [];
-        for (var i = 0; i < queue.length; i++) {
-          if (queue[i].leftChild) {
-            newQueue.push(queue[i].leftChild);
-          }
-          if (queue[i].rightChild) {
-            newQueue.push(queue[i].rightChild);
-          }
-          callback(queue[i]);
-        }
-        queue = newQueue;
-      }
-    }
-
-    self.contains = function(callback, traversal) {
-      traversal = typeof traversal !== 'undefined' ? traversal : self.traverseDF;
-
-      traversal.call(self, callback);
-    }
-
-    self.addChild = function(data, toData, traversal) {
-      var child = new Node(data),
-        parent = null,
-        callback = function(node) {
-          if (node.data === toData) {
-            parent = node;
-          }
-        };
-
-      this.contains(callback, traversal);
-
-      if (parent === null) {
-        throw "Error: you tried to add a child to a non-existend node";
-      } else {
-        parent.addChild(child);
-        child.parent = parent;
-      }
-    }
-    
-    self.compute = function() {
-      return (function aux(node) {
-        if (! node.leftChild && ! node.rightChild) {
-          return parseInt(node.data);
-        } else if (! node.leftChild || ! node.rightChild) {
-          throw "Error during computation: a node has one and only one child.";
-        } else {
-          var a = aux(node.leftChild),
-              b = aux(node.rightChild),
-              result;
-          switch (node.data) {
-            case "+":
-              result = a + b;
-              break;
-            case "*":
-              result = a * b;
-              break;
-            case "/":
-              result = a / b;
-              break;
-            case "-":
-              result = a - b;
-              break;
-            default:
-              throw "Unknow operator";
-          }
-          return result;
-        }
-      })(self._root);
-    }
-    
-    
-    // à revoir
-    self.isValidCalculationTree = function() {
-      var rNode = /^[\+\-\*\/]{1}$/;
-
-      (function aux(node) {
-
-        if (node.leftChild && node.rightChild) { // Bien des enfants, on vérifie que c'est un opérateur
-
-          if (!rNode.test(node.data)) {
-            return false;
-          } else {
-            return aux(node.leftChild) && aux(node.rightChild);
-          }
-
-        } else if ((node.leftChild && !node.rightChild) || (!node.leftChild && node.rightChild)) {
-
-          return false; // Un seul fils, pas possible car on a pas implémenté les fonctions
-
-        } else { // Pas d'enfant, ça doit être un nombre
-
-          return ! isNaN(node.data);
-
-        }
-      })(self._root);
-    }
-  }
-
-  function auxTreeFromTokens(parent, tokens) {
-    if (tokens.length === 0) { // Problem
-      throw "Empty token list in auxTreeFromTokens";
-    } else if (tokens.length === 1) { // Ending scenario, only a number remains
-      if (isNaN(tokens[0])) {
-        throw "Invalid expression in auxTreeFromTokens : leaf with operator.";
-      } else {
-        var leaf = new Node(tokens[0]);
-        leaf.parent = parent;
-        return leaf;
-      }
+  
+  function intCompare(a, b) {
+    if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return 1;
     } else {
-      var nodeIndex = null;
-      for (var i = 0; i < tokens.length; i++) {
-        if (nodeIndex === null) {
-          if (tokens[i] in priorities) {
-            nodeIndex = i;
-          }
-        } else if (tokens[i] in priorities && priorities[tokens[i]] < priorities[tokens[nodeIndex]]) {
-          nodeIndex = i;
-        }
-      }
-      var tree = new Tree(tokens[nodeIndex]);
-      tree._root.parent = parent;
-      tree._root.leftChild = auxTreeFromTokens(tree._root, tokens.slice(0, nodeIndex));
-      tree._root.rightChild = auxTreeFromTokens(tree._root, tokens.slice(nodeIndex + 1, tokens.length));
-      return tree._root;
+      return 0;
     }
   }
-
-  function treeFromTokens(tokens) {
+  
+  function sameLevelCompute(tokens) {
+    console.log("Sub computation bitch");
+    console.log("Sequence computed : ");
+    console.log(tokens);
+    var result = parseInt(tokens[0]);
+    console.log("Initialized result : " + result);
+    for (var j = 1; j < tokens.length; j += 2) {
+      console.log("J : " + j);
+      switch (tokens[j]) {
+        case "*":
+          result *= parseInt(tokens[j + 1]);
+          break;
+        case "/":
+          result /= parseInt(tokens[j + 1]);
+          break;
+        case "+":
+          result += parseInt(tokens[j + 1]);
+          break;
+        case "-":
+          result -= parseInt(tokens[j + 1]);
+          break;
+        default:
+          throw "Unknown operator encountered druing sub-computation.";
+      }
+      console.log("Temp result : " + result);
+    }
+    console.log("Result : " + result);
+    return result;
+  }
+  
+  // Assumes that tokens is a valid token list created by the lexer function which whecks validity.
+  function compute(tokens) {
     if (tokens.length === 0) {
-      throw "Empty token list passed to treeFromTokens";
-    } else if (tokens.length === 1) {
-      return new Tree(tokens[0]);
+      throw "Empty tokens list";
     }
-
-    var nodeIndex = null;
+    if (tokens.length === 1) {
+      return parseInt(tokens[0]);
+    }
+    
+    var result = 0;
+    
+    // Get all priority levels to compute in the good order
+    var priorityLevels = [];
     for (var i = 0; i < tokens.length; i++) {
-      if (nodeIndex === null) {
-        if (tokens[i] in priorities) {
-          nodeIndex = i;
-        }
-      } else if (tokens[i] in priorities && priorities[tokens[i]] < priorities[tokens[nodeIndex]]) {
-        nodeIndex = i;
+      if (tokens[i] in priorities && priorityLevels.indexOf(priorities[tokens[i]]) == -1) {
+        priorityLevels.push(priorities[tokens[i]]);
       }
     }
-    var finalTree = new Tree(tokens[nodeIndex]);
-    finalTree._root.leftChild = auxTreeFromTokens(finalTree._root, tokens.slice(0, nodeIndex));
-    finalTree._root.rightChild = auxTreeFromTokens(finalTree._root, tokens.slice(nodeIndex + 1, tokens.length));
-    return finalTree;
+    priorityLevels = priorityLevels.sort();
+    
+    // On traite les levels un par un en remplaçant comme il faut
+    var tempRes;
+    var sequenceBeginning = -1;
+    var lastSequenceEnd = 0;
+    var len = priorityLevels.length;
+    for (i = 0; i < len; i++) {
+      console.log("Hello ?");
+      var bufferArray = [];
+      var priority = priorityLevels[len - i - 1];
+      console.log("Current priority : " + priority);
+      for (var j = 1; j < tokens.length; j += 2) {
+        console.log("j : " + j);
+        console.log("Token : " + tokens[j]);
+        if (tokens[j] in priorities && priorities[tokens[j]] == priority) {
+          console.log("Token de bonne priorité");
+          if (sequenceBeginning == -1) {
+            console.log("Début de séquence");
+            sequenceBeginning = j - 1;
+          }
+        } else {
+          console.log("Token de mauvaise priorité");
+          if (sequenceBeginning != -1) { // On a une séquence à calculer
+            console.log("Fin de séquence");
+            bufferArray = bufferArray.concat(tokens.slice(lastSequenceEnd, sequenceBeginning));
+            tempRes = sameLevelCompute(tokens.slice(sequenceBeginning, j));
+            result += tempRes;
+            bufferArray.push(tempRes.toString());
+            sequenceBeginning = -1;
+            lastSequenceEnd = j;
+          }
+        }
+      }
+      if (sequenceBeginning != -1) { // Cas où on a une seq en fin de liste
+        console.log("Séquence en fin de liste");
+        tempRes = sameLevelCompute(tokens.slice(sequenceBeginning, j));
+        result += tempRes;
+        bufferArray.push(tempRes.toString());
+      } else {
+        bufferArray = bufferArray.concat(tokens.slice(lastSequenceEnd, tokens.length));
+      }
+      console.log("New token list : " + bufferArray);
+      tokens = bufferArray;
+      if (len - i - 1 !== 0) {
+        result = 0;
+      }
+    }
+    
+    return result;
   }
-
-  var tokens = lexer("10 + 5 * 9 * 11 + 13 + 27")
+  
+  var tokens = lexer("58  * 12 + 14 * 3");
+  
+  console.log("Tokens : ");
   console.log(tokens);
-  var tree = treeFromTokens(tokens);
-  console.log(tree);
-  console.log(tree.compute());
+  console.log(compute(tokens));
 
 });
